219a665f66e75366240facb5b3ad91fe6946483c
Arnold Rimer <pavelsixta2@seznam.cz>
2020-10-20 20:43:06 +0200

    upravy1

--- js/game.js
+++ js/game.js
@@ -1,30 +1,37 @@
-let hody = [];
+let throws = [];
 let timer = false;
 let h = 1;
-//button = document.getElementById(game);
+button = document.getElementById("game");
+dice = document.getElementById('cube');
+result = document.getElementById('result')
 
 function animation() {
     h = Math.ceil(Math.random() * 6);
-    document.getElementById('cube').src='img/kostka' + h + '.png';
+    dice.src='img/kostka' + h + '.png';
 }
 
-document.getElementById('game').addEventListener('click',
+
+
+
+button.addEventListener('click',
     function(){
         if (!timer) {
+            sound();
             timer = setInterval(animation, 50);
-            document.getElementById('game').innerHTML = "STOP";
+            button.innerHTML = "STOP";
         }
         else {
             clearInterval (timer);
             timer = false;
-            document.getElementById('game').innerHTML = "PLAY";
+            button.innerHTML = "PLAY";
             hod();
-            console.log(hody);
-        }
-       
+            
+            console.log(throws);
+        } 
         
-    }
-);
+    });
+
+    
 
 function suma(cisla) {
     let sum = 0;
@@ -55,19 +62,30 @@ function average(sum, count) {
 }
 
 function hod() {
+
+    throws.push(h);
     
-    hody.push(h);
-    
-    document.getElementById('result').innerHTML = '<p>Hod: ' + h + '</p>';
-    document.getElementById('result').innerHTML += 
-        '<p>Number of throws: ' + hody.length + '</p>';
-    document.getElementById('result').innerHTML += 
-        '<p>Sum of throws: ' + suma(hody) + '</p>';
-    document.getElementById('result').innerHTML += 
-        '<p>Avrage of throws: ' + average(suma(hody),hody.length) + '</p>';
-    document.getElementById('result').innerHTML += 
-        '<p>Best throw: ' + maximum(hody) + '</p>';
-    document.getElementById('result').innerHTML += 
-        '<p>Worst throw: ' + minimum(hody) + '</p>';
+    result.innerHTML = '<p>Hod: ' + h + '</p>';
+    result.innerHTML += 
+        '<p>Number of throws: ' + throws.length + '</p>';
+    result.innerHTML += 
+        '<p>Sum of throws: ' + suma(throws) + '</p>';
+    result.innerHTML += 
+        '<p>Avrage of throws: ' + average(suma(throws),throws.length) + '</p>';
+    result.innerHTML += 
+        '<p>Best throw: ' + maximum(throws) + '</p>';
+    result.innerHTML += 
+        '<p>Worst throw: ' + minimum(throws) + '</p>';
     return h;
+}
+
+
+function sound (){
+
+        let sound = document.getElementById("pavel");
+        sound.currentTime = 0;
+        sound.play();
+        window.setTimeout(function(){
+            sound.pause();
+        }, 4000);
 }
